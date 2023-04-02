9f6bc1cb99f1630e93eeb0751f56e191fa2600af
tristan <tristanaaron.2711@gmail.com>
2023-04-02 20:11:03 +0700

    Choose Map Frame Update

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -26,9 +26,9 @@ public class Constants {
         public static String BACKGROUND_GAME_2 = "/res/sprite/Main-Background-2-Cloud.png";
         public static String BUTTON_MAINMENU = "/res/sprite/Button-MainMenu.png";
         public static String MAINMENU_PANEL = "/res/sprite/Button-MainMenu-Panel-2.png";
-        public static String MAP_1 = "/res/sprite/Main-Background.jpeg";
-        public static String MAP_1_HVR = "/res/sprite/Main-Background-Hover.jpeg";
-        public static String MAP_1_CLK = "/res/sprite/Main-Background-Click.jpeg";
+        public static String MAP_1 = "/res/sprite/Main-Background.png";
+        public static String MAP_1_HVR = "/res/sprite/Main-Background-Hover.png";
+        public static String MAP_1_CLK = "/res/sprite/Main-Background-Click.png";
         public static String MAP_2 = "/res/sprite/Main-Background-2.png";
         public static String MAP_2_HVR = "/res/sprite/Main-Background-2-Hover.png";
         public static String MAP_2_CLK = "/res/sprite/Main-Background-2-Click.png";
